5fbcbe15f4eb2f6a7a338835ca584424704c2151
Vitoria Fortes <fortesvitoria@gmail.com>
2026-03-24 21:28:28 -0300

    add correcao prof

--- Aula03ExerciciosJavaScript/aula03ex01Prof.js
+++ Aula03ExerciciosJavaScript/aula03ex01Prof.js
@@ -17,5 +17,14 @@ const prompt = require('prompt-sync')();
 
 let numero = prompt('Digite um número inteiro de três dígitos: ');
 
+let unidade = numero % 10; // Extrai a unidade
+let dezena = Math.floor((numero % 100) / 10); // Extrai a dezena
+let centena = Math.floor(numero / 100); // Extrai a centena
+let soma = unidade + dezena + centena; // Soma dos dígitos
+
+console.log(`Unidade: ${unidade}`);
+console.log(`Dezena: ${dezena}`);
+console.log(`Centena: ${centena}`);
+console.log(`Soma dos dígitos: ${soma}`);     
 
 
