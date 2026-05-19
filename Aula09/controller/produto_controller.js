35912b861d93d96dfaad74ac2bf8043094be3bdb
Vitoria Fortes <fortesvitoria@gmail.com>
2026-05-19 20:07:46 -0300

    atualiza controller

--- Aula09/controller/produto_controller.js
+++ Aula09/controller/produto_controller.js
@@ -5,6 +5,32 @@ async function listar(req, res) {
     res.json(listaProdutos);
 }
 
+async function buscarPorId(req, res) {
+    const id = req.params.id;
+    try {
+        const produto = await produtoService.buscarPorId(id);
+        res.json(produto);
+    }
+    catch (err) {
+        res.status(err.id).json({ erro: err.msg });
+    }
+}
+
+async function inserir(req, res) {
+    const produto = req.body;
+    try{
+        const produtoInserido = await produtoService.inserir(produto);
+        res.status(201).json(produtoInserido);
+    }
+    catch(err) {
+        res.status(err.id).json({erro:err.msg});
+    }
+
+    
+}
+
 module.exports = {
-    listar
+    listar,
+    buscarPorId,
+    inserir
 }
